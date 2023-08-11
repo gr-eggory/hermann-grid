02cc33af6557c64a032a1f5b99575c3e1f0e26c0
gr-eggory <greggroks@gmail.com>
2023-08-11 14:25:41 -0400

    Resizing canvas

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import "./style.css";
 
-const main = () => {
-  const canvas = document.getElementById("hermann-grid") as HTMLCanvasElement;
-  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+const resizeCanvas = (canvas: HTMLCanvasElement) => {
+  console.log("running");
   const container = document.querySelector(
     ".canvas-container"
   ) as HTMLDivElement;
 
   const containerRect = container.getBoundingClientRect();
   canvas.width = containerRect.width;
+};
 
+const setupCanvasSizing = (canvas: HTMLCanvasElement) => {
+  resizeCanvas(canvas);
+  window.addEventListener("resize", () => resizeCanvas(canvas));
+};
+
+const main = () => {
+  const canvas = document.getElementById("hermann-grid") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  setupCanvasSizing(canvas);
 };
 
 main();
