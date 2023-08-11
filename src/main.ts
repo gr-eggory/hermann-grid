a6805de532cc157e2d14023730af6b33d84e5565
gr-eggory <greggroks@gmail.com>
2023-08-11 14:12:16 -0400

    Basic Canvas setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const main = () => {
+  const canvas = document.getElementById("hermann-grid") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+  ctx.fillRect(40, 40, 100, 100);
+};
+
+main();
