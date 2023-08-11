372213f15e049e4349c2d1ea9464047faf3deccb
gr-eggory <greggroks@gmail.com>
2023-08-11 14:50:50 -0400

    Adding as many squares as can fit

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,22 @@ import { setupCanvasSizing } from "./logic/canvasSizing";
 const main = () => {
   const canvas = document.getElementById("hermann-grid") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-
   setupCanvasSizing(canvas);
+
+  const squareDimension = 50;
+  const gap = 10;
+
+  const amountOfSquaresX = Math.floor(canvas.width / (squareDimension + gap));
+  const amountOfSquaresY = Math.floor(canvas.height / (squareDimension + gap));
+
+  for (let i = 0; i < amountOfSquaresY; i++) {
+    const y = (squareDimension + gap) * i;
+
+    for (let j = 0; j < amountOfSquaresX; j++) {
+      const x = (squareDimension + gap) * j;
+      ctx.fillRect(x, y, squareDimension, squareDimension);
+    }
+  }
 };
 
 main();
