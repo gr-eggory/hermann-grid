2d5617aaef85d3368fb17dbe9e009579f7ca2e30
gr-eggory <greggroks@gmail.com>
2023-09-17 16:01:26 -0400

    Added event listeners

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 import "./style.css";
 
-import { setupHermanGrid } from "./logic/canvas";
+import { updateHermanGrid, setupHermanGrid } from "./logic/canvas";
 
 const main = () => {
+  const squareDimensionInput = document.getElementById(
+    "square-dimension"
+  ) as HTMLInputElement;
+  const gapDimensionInput = document.getElementById(
+    "gap-size"
+  ) as HTMLInputElement;
+
   const canvas = document.getElementById("hermann-grid") as HTMLCanvasElement;
+
+  const config = {
+    squareDimension: Number(squareDimensionInput.value),
+    gap: Number(gapDimensionInput.value),
+  };
+
   setupHermanGrid(canvas);
+
+  squareDimensionInput.addEventListener("input", (event) => {
+    updateHermanGrid(event, canvas, config);
+  });
+
+  gapDimensionInput.addEventListener("input", (event) => {
+    updateHermanGrid(event, canvas, config);
+  });
 };
 
 main();
